972984cd9d88f693bdcf9fbe9f3bfb3a2afedf06
neilhacker <39994403+neilhacker@users.noreply.github.com>
2022-01-26 12:25:37 +0000

    changed formatting to work with narrower screen

--- helpers.js
+++ helpers.js
@@ -1,32 +1,4 @@
 
-
-/*function to calculate cumulative cost of scale increases*/
-// SUM[n,k] C1 k^log2(b) 
-// k is start cumulative volume
-//  n is end cumuluative volume
-function cumulativeCost(k, n, c1, l) {
-    const b = 1- l/100
-    Qb = Math.ceil(n)
-
-    console.log(k)
-    console.log(Qb)
-    console.log(c1)
-    console.log(Math.log2(b))
-
-    var totalCost = 0
-    for (let i = k; i < Qb+1; i++) {
-        var unitCost =  i ** Math.log2(b)
-        totalCost += unitCost
-    }
-    totalCost = totalCost * c1
-    totalCost = Math.ceil(totalCost)
-    // document.getElementById("scaleCost").innerHTML = totalCost;
-    document.getElementById("scaleCost").innerHTML = "$"+totalCost.toLocaleString();
-    document.getElementById("scaleCostMargin").innerHTML = "$"+totalCost.toLocaleString();
-
-
-}
-
 /*function to hide/show sidebar*/
 function sidebar(idTag) {
     console.log(idTag)
@@ -37,7 +9,23 @@ function sidebar(idTag) {
             if (elms[i].style.display === "block") {
                 elms[i].style.display = "none";
             } else {
+                console.log("bye", window.outerWidth)  
                 elms[i].style.display = "block";
+                if (window.outerWidth <= 660) {   
+                    console.log("hi")  
+                    elms[i].style.left = "1rem"
+                    elms[i].style.width = "95%"
+                    elms[i].style.margin = "1rem 2.5%"
+                  } else {
+                    elms[i].style.left = "none"
+                    elms[i].style.width = "50%"
+                    elms[i].style.margin = "none"
+                    elms[i].style.marginRight = "-60%"
+                    elms[i].style.marginTop = "0.3rem"
+                    elms[i].style.marginBottom = "0"
+
+                  }
+            
             }
         } else {
             elms[i].style.display = "none";
@@ -45,56 +33,13 @@ function sidebar(idTag) {
     }
 }
 
-/*function to highlight all elements of same class on hover*/
-function hoverByClass(classname,colorover,colorout="transparent"){
-    var elms=document.getElementsByClassName(classname);
-    for(var i=0;i<elms.length;i++){
-        elms[i].onmouseover = function(){
-            for(var k=0;k<elms.length;k++){
-                if(elms[k].tagName=="VAL-IN-WORDS"){
-                    elms[k].style.backgroundColor="#5F9AB6";
-                } else {
-                    elms[k].style.backgroundColor=colorover;
-                }
-            }
-        };
-        elms[i].onmouseout = function(){
-            for(var k=0;k<elms.length;k++){
-                if(elms[k].tagName=="VAL-IN-WORDS"){
-                    elms[k].style.backgroundColor="#91B9CC";
-                } else {
-                    elms[k].style.backgroundColor=colorout;
-                }
-            }
-        };
-    }
-}
 
-
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-        /*make an HTTP request using the attribute value as the file name:*/
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /*remove the attribute, and call this function once more:*/
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-            }
-        }      
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /*exit the function:*/
-        return;
-        }
-    }
-};
+    /* left: 1rem;
+    width: 95%;
+    margin: 1rem 2.5%; */
+    // if ( $(window).width() <= 660) {     
+    //     //Add your javascript for screens wider than or equal to 768 here
+    //   }
+    //   else {
+    //     //Add your javascript for screens smaller than 768 here
+    //   }
--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll(".sidebar  ul li");
+const main_sidebar = document.querySelectorAll(".sidebar_main  ul li");
+const expandable_sidebar = document.querySelectorAll(".sidebar_thin  ul li");
+const header = document.getElementById("header_text");
 
 window.addEventListener("scroll", () => {
   let current = "";
+  // console.log("window", window.innerWidth)
+  // console.log("window", window.outerWidth)
+
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
     if (scrollY >= sectionTop - sectionHeight/10) {
       current = section.getAttribute("id");
+    }  
+  });
+
+// console.log("header", header.innerHTML)
+header.innerHTML = current
+
+main_sidebar.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
     }
   });
 
-  navLi.forEach((li) => {
+
+  expandable_sidebar.forEach((li) => {
     li.classList.remove("active");
     if (li.classList.contains(current)) {
       li.classList.add("active");
     }
   });
 });
+
+
 
